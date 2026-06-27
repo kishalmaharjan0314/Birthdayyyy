fa963b02fda6ba5a81dc37adc00cf6352919698c
Kishal Maharjan <kishalmaharjan@gmail.com>
2026-06-27 18:13:39 +0545

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ function finalPage(){
     document.getElementById("final").style.display="block";
 
 }
+unction openImage(src){
+
+    document.getElementById("popup").style.display = "flex";
+
+    document.getElementById("popup-img").src = src;
+
+}
+
+function closeImage(){
+
+    document.getElementById("popup").style.display = "none";
+
+}
