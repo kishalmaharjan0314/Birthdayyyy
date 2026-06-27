02b932c764cc43be895252db054ca179148d5bcc
Kishal Maharjan <kishalmaharjan@gmail.com>
2026-06-27 18:40:25 +0545

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,17 @@ function closeImage(){
     document.getElementById("imageViewer").style.display="none";
 }
 
+setInterval(() => {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.innerHTML = "❤️";
+    heart.style.left = Math.random()*100 + "vw";
+    heart.style.fontSize = (20 + Math.random()*20) + "px";
+
+    document.body.appendChild(heart);
+
+    setTimeout(()=>{
+        heart.remove();
+    },8000);
+
+},500);
